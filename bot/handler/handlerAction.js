65462527f4a0e42b8930627e6a2fd3343e1620f7
Nice2hh <absm4607@gmail.com>
2025-08-30 18:46:16 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "🫠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61578034136500"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61580068018277"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
